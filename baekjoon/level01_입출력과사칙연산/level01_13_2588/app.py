7f51188e2883f9365c6f18d60940b6c426fedefb
mameMan <jokingbear01@gmail.com>
2022-03-13 18:25:19 +0900

    level01

---
+++ baekjoon/level01_입출력과사칙연산/level01_13_2588/app.py
@@ -0,0 +1,11 @@
+numA = int(input())
+numB = input()
+
+numBH = int(numB[0])
+numBT = int(numB[1])
+numBO = int(numB[2])
+
+print(numA * numBO)
+print(numA * numBT)
+print(numA * numBH)
+print(numA * int(numB))
