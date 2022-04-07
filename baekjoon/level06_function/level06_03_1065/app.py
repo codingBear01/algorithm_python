976d8f23a3cbc740d4bd270be1c075140f83237c
mameMan <jokingbear01@gmail.com>
2022-04-07 23:31:06 +0900

    Update app.py

--- baekjoon/level06_function/level06_03_1065/app.py
+++ baekjoon/level06_function/level06_03_1065/app.py
@@ -1,28 +1,49 @@
-inputN = input()
+# inputN = input()
 
 
-def hansu(num):
-    strNum = str(num)
+# def hansu(num):
+#     strNum = str(num)
 
-    if len(strNum) <= 2:
-        return True
+#     if len(strNum) <= 2:
+#         return True
 
-    diff = int(strNum[1]) - int(strNum[0])
-    beforeNum = int(strNum[1])
+#     diff = int(strNum[1]) - int(strNum[0])
+#     beforeNum = int(strNum[1])
 
-    for i in range(2, len(strNum)):
-        if int(strNum[i]) - beforeNum != diff:
-            return False
-        beforeNum = int(strNum[i])
-    return True
+#     for i in range(2, len(strNum)):
+#         if int(strNum[i]) - beforeNum != diff:
+#             return False
+#         beforeNum = int(strNum[i])
+#     return True
 
 
-def solution(N):
+# def solution(N):
+#     cnt = 0
+#     for i in range(1, int(N) + 1):
+#         if hansu(i):
+#             cnt += 1
+#     print(cnt)
+
+
+# solution(inputN)
+
+
+num = int(input())
+
+
+def hansu(n):
     cnt = 0
-    for i in range(1, int(N) + 1):
-        if hansu(i):
+    for i in range(1, n + 1):
+        hundred = i // 100
+        ten = (i - 100 * hundred) // 10
+        one = i % 10
+
+        if i <= 99:
             cnt += 1
-    print(cnt)
+        elif 100 <= i and i < 1000:
+            if hundred - ten == ten - one:
+                cnt += 1
+    return cnt
 
 
-solution(inputN)
+print(hansu(num))
